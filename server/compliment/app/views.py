d3d193347c0bca76f9d6694790c3a509d1dd8bec
FilipStefaniuk <filip.stefaniuk@gmail.com>
2018-05-20 00:07:47 +0200

    nearby users

--- server/compliment/app/views.py
+++ server/compliment/app/views.py
@@ -2,6 +2,7 @@ from app.models import Location, Pin, User
 from app.serializers import LocationSerializer, PinSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class UserListCreate(generics.ListCreateAPIView):
@@ -19,7 +20,22 @@ class PinList(generics.ListCreateAPIView):
     serializer_class = PinSerializer
 
 
-# class NearbyUsersList(APIView):
-#
-#     def get(self, request, format=None):
-#         request
+class NearbyUsersList(APIView):
+
+    def get(self, request, format=None):
+
+        dist = float(request.GET['dist'])
+        user_id = request.GET['user_id']
+        user_loc = Location.objects.filter(user_id=user_id).latest('timestamp')
+
+        users = User.objects.all().exclude(auto_id=user_id)
+        locations = [Location.objects.filter(user=user).latest('timestamp') for user in users]
+
+        nearby_users = []
+
+        for user, location in zip(users, locations):
+            if (abs(location.latitude - user_loc.latitude) < dist) and (abs(location.longitude - user_loc.longitude) < dist):
+                nearby_users.append(user)
+
+        serializer = UserSerializer(nearby_users, many=True)
+        return Response(serializer.data)
--- server/compliment/urls.py
+++ server/compliment/urls.py
@@ -5,5 +5,6 @@ from compliment.app import views
 urlpatterns = [
     url(r'^locations/', views.LocationList.as_view()),
     url(r'^pins/', views.PinList.as_view()),
-    url(r'^users/', views.UserListCreate.as_view())
+    url(r'^users/', views.UserListCreate.as_view()),
+    url(r'^nearby_users/', views.NearbyUsersList.as_view())
 ]
