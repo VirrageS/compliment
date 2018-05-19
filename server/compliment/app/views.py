22fdd6fdd2519c1a05ab5ba79d5744a39075fdcb
Iwona Pytel <iwopyt@gmail.com>
2018-05-19 15:58:23 +0200

    basic server

---
+++ server/compliment/app/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from app.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
